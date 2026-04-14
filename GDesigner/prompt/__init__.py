e2d52c0d349ea1ae426c0f8c9fca0e2adf47a8b4
yuhuo03 <122090203@link.cuhk.edu.cn>
2026-04-14 19:17:49 +0800

    Register new math prompt sets

--- GDesigner/prompt/__init__.py
+++ GDesigner/prompt/__init__.py
@@ -2,8 +2,14 @@ from GDesigner.prompt.prompt_set_registry import PromptSetRegistry
 from GDesigner.prompt.mmlu_prompt_set import MMLUPromptSet
 from GDesigner.prompt.humaneval_prompt_set import HumanEvalPromptSet
 from GDesigner.prompt.gsm8k_prompt_set import GSM8KPromptSet
+from GDesigner.prompt.multiarith_prompt_set import MultiArithPromptSet
+from GDesigner.prompt.svamp_prompt_set import SVAMPPromptSet
+from GDesigner.prompt.aqua_prompt_set import AQUAPromptSet
 
 __all__ = ['MMLUPromptSet',
            'HumanEvalPromptSet',
            'GSM8KPromptSet',
+           'MultiArithPromptSet',
+           'SVAMPPromptSet',
+           'AQUAPromptSet',
            'PromptSetRegistry',]
