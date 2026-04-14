079860f739beaa97579386955633b0998ec5c206
yuhuo03 <122090203@link.cuhk.edu.cn>
2026-04-14 19:17:49 +0800

    Add shared logger utility

---
+++ GDesigner/utils/log.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S',
+    stream=sys.stdout,
+)
+logger = logging.getLogger("GDesigner")
